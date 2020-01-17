4778db84a5fc4e3a4f4e86642acd261ec3bc54a1
James Taylor <jamest@uk.ibm.com>
2020-01-17 14:34:44 +0000

    Update small network
    Enable tls
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- scripts/mknet.sh
+++ scripts/mknet.sh
@@ -16,10 +16,10 @@ function error_exit()
 source /etc/hyperledger/fabric/.fdnrc
 
 # Create channel
-peer channel create -o orderer.example.com:7050 -c ${CHANNEL_NAME} -f /etc/hyperledger/fabric/configtx/channel.tx --outputBlock /etc/hyperledger/fabric/configtx/channel.block
+peer channel create -o orderer.example.com:7050 --tls ${CORE_PEER_TLS_ENABLED} --cafile ${ORDERER_CA_FILE} -c ${CHANNEL_NAME} -f /etc/hyperledger/fabric/configtx/channel.tx --outputBlock /etc/hyperledger/fabric/configtx/channel.block
 
 # Join peer to channel
 peer channel join -b /etc/hyperledger/fabric/configtx/channel.block
 
 # Update channel
-peer channel update -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c ${CHANNEL_NAME} -f /etc/hyperledger/fabric/configtx/${CORE_PEER_LOCALMSPID}anchors.tx
+peer channel update -o orderer.example.com:7050 --tls ${CORE_PEER_TLS_ENABLED} --cafile ${ORDERER_CA_FILE} -c ${CHANNEL_NAME} -f /etc/hyperledger/fabric/configtx/${CORE_PEER_LOCALMSPID}anchors.tx
